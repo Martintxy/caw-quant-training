22c42b64abf4e3cf0b21dc2605001921fa325be2
Martin Tong <martintxy@gmail.com>
2020-07-17 10:55:37 -0400

    Update test.py

--- section1/task1/test.py
+++ section1/task1/test.py
@@ -4,5 +4,17 @@ import datetime
 import numpy as np
 import pandas as pd
 import math
-a = pd.to_datetime(1490195805, unit='s')
-print(a)
+limit = 10
+tsym = "USDT"
+# req = requests.get(
+#             'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USDT')
+# df = pd.DataFrame.from_dict(req.json(), orient = 'index')
+# df_data = df.loc["Data",0]
+# print(df_data)
+
+req = requests.get('https://min-api.cryptocompare.com/data/top/pairs?fsym=BTC&limit=10')
+# convert the json type object into a dataframe
+df = pd.DataFrame.from_dict(req.json(), orient = 'index')
+df_table = pd.DataFrame(df.loc["Data", 0])
+print(df_table)
+
