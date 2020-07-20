18fa383daac0e1b1695baad2764f363c69926757
Martin Tong <martintxy@gmail.com>
2020-07-20 15:58:51 -0400

    1.1 updated 7/20

--- section1/task1/1.1.py
+++ section1/task1/1.1.py
@@ -5,19 +5,28 @@ import numpy as np
 import pandas as pd
 import math
 
+def data_formatting(df_table):
+    # formatting
+    df_table = df_table.drop(
+        columns=['conversionType', 'conversionSymbol'])
+    df_table = df_table.reindex(
+        columns=['close', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'time'])
+    df_table = df_table.rename(
+        columns={'volumefrom': 'volume', 'volumeto': 'baseVolume', 'time': 'datetime'})
+    df_table['datetime'] = pd.to_datetime(df_table['datetime'], unit='s')
+    return(df_table)
 
 class CryptoCompareAPI():
-    # constuctor
     def __init__(self):
         self.url = 'https://min-api.cryptocompare.com/data'
 
     def get_histohour_data(self, fsym, tsym, e, start_time, end_time):
-        # convert datetime to unix epoch time
+        # convert datetime to unix
         start_time_unix = pd.Timestamp(start_time).value // 10**9
         end_time_unix = pd.Timestamp(end_time).value // 10**9
         # request from cryptocompare
-        req = requests.get(
-            'https://min-api.cryptocompare.com/data/v2/histohour?fsym={}&tsym={}&e={}&limit=2000&toTs={}'.format(fsym, tsym, e, end_time_unix))
+        histohour_url_first = '/v2/histohour?fsym={}&tsym={}&e={}&limit=2000&toTs={}'.format(fsym, tsym, e, end_time_unix)
+        req = requests.get(self.url + histohour_url_first)
         # convert the json type object into a dataframe
         df = pd.DataFrame(req.json())
         # get the data table for one request
@@ -25,15 +34,14 @@ class CryptoCompareAPI():
         # get the timestamp for the subsequent request
         df_timestamploop = df.loc["TimeFrom", "Data"]
 
-        # write for loop to request data repeatedly
+        # for loop to request data
         hours = int((end_time_unix - start_time_unix) / 3600)
         time_of_request = int(hours // 2000)
         limit = int(hours % 2000)
-        # no need to +1 because it is do-while
         for i in range(1, time_of_request):
             # request according to the last timestamp
-            req_loop = requests.get(
-                'https://min-api.cryptocompare.com/data/v2/histohour?fsym={}&tsym={}&e={}&limit=2000&toTs={}'.format(fsym, tsym, e, df_timestamploop))
+            histohour_url_loop = '/v2/histohour?fsym={}&tsym={}&e={}&limit=2000&toTs={}'.format(fsym, tsym, e, df_timestamploop)
+            req_loop = requests.get(self.url + histohour_url_loop)
             df_loop = pd.DataFrame(req_loop.json())
             # extract the table and convert to a dataframe
             df_tableloop = pd.DataFrame(df_loop.loc["Data", "Data"])
@@ -43,8 +51,8 @@ class CryptoCompareAPI():
             df_table = pd.concat([df_tableloop, df_table]).drop_duplicates()
 
         # get last request < 2000 hour
-        req_loop = requests.get(
-            'https://min-api.cryptocompare.com/data/v2/histohour?fsym={}&tsym={}&e={}&limit={}&toTs={}'.format(fsym, tsym, e, limit, df_timestamploop))
+        histohour_url_final = '/v2/histohour?fsym={}&tsym={}&e={}&limit={}&toTs={}'.format(fsym, tsym, e, limit, df_timestamploop)
+        req_loop = requests.get(self.url + histohour_url_final)
         df_loop = pd.DataFrame(req_loop.json())
         df_tableloop = pd.DataFrame(df_loop.loc["Data", "Data"])
         df_timestamploop = df_loop.loc["TimeFrom", "Data"]
@@ -52,23 +60,18 @@ class CryptoCompareAPI():
         df_table = pd.concat([df_tableloop, df_table]).drop_duplicates()
 
         # formatting
-        df_table = df_table.drop(
-            columns=['conversionType', 'conversionSymbol'])
-        df_table = df_table.reindex(
-            columns=['close', 'high', 'low', 'open', 'volumefrom', 'volumeto', 'time'])
-        df_table = df_table.rename(
-            columns={'volumefrom': 'volume', 'volumeto': 'baseVolume', 'time': 'datetime'})
-        df_table['datetime'] = pd.to_datetime(df_table['datetime'], unit='s')
+        df_table = data_formatting(df_table)
         return(df_table)
 
     # optional
     def get_toplist_trading_pairs(self, fsym, limit):
-        req = requests.get(
-            'https://min-api.cryptocompare.com/data/top/pairs?fsym={}&limit={}'.format(fsym, limit))
+        toplist_url = '/top/pairs?fsym={}&limit={}'.format(fsym, limit)
+        req = requests.get(self.url + toplist_url)
         # convert the json type object into a dataframe
-        df = pd.DataFrame.from_dict(req.json(), orient = 'index')
+        df = pd.DataFrame.from_dict(req.json(), orient='index')
         df_table = pd.DataFrame(df.loc["Data", 0])
         return(df_table)
+
 
 # test
 fsym = "BTC"
@@ -77,10 +80,11 @@ e = "binance"
 start_time = "2017-04-01 00:00:00"
 end_time = "2020-04-01 00:00:00"
 candle_data = CryptoCompareAPI()
-histohour_datatable = candle_data.get_histohour_data(fsym, tsym, e, start_time, end_time)
+histohour_datatable = candle_data.get_histohour_data(
+    fsym, tsym, e, start_time, end_time)
 print(histohour_datatable)
 # export to .csv file and remove the row index
-histohour_datatable.to_csv('1.1.csv', index = False)
+histohour_datatable.to_csv('1.1.csv', index=False)
 
 # optional
 limit = 10
--- section1/task1/test.py
+++ section1/task1/test.py
@@ -11,10 +11,16 @@ tsym = "USDT"
 # df = pd.DataFrame.from_dict(req.json(), orient = 'index')
 # df_data = df.loc["Data",0]
 # print(df_data)
+fsym = "BTC"
+tsym = "USDT"
+e = "binance"
+start_time = "2017-04-01 00:00:00"
+end_time = "2020-04-01 00:00:00"
+url = 'https://min-api.cryptocompare.com/data'
+start_time_unix = pd.Timestamp(start_time).value // 10**9
+end_time_unix = pd.Timestamp(end_time).value // 10**9
+# request from cryptocompare
+histohour_url_first = '/v2/histohour?fsym={}&tsym={}&e={}&limit=2000&toTs={}'.format(fsym, tsym, e, end_time_unix)
+print(url + histohour_url_first)
 
-req = requests.get('https://min-api.cryptocompare.com/data/top/pairs?fsym=BTC&limit=10')
-# convert the json type object into a dataframe
-df = pd.DataFrame.from_dict(req.json(), orient = 'index')
-df_table = pd.DataFrame(df.loc["Data", 0])
-print(df_table)
 
