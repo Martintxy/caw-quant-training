bebd0ea3b633325c72451b71c75703cc42cee54c
Martin Tong <martintxy@gmail.com>
2020-07-23 12:52:44 -0400

    1.2 revised

--- section1/task1/test.py
+++ section1/task1/test.py
@@ -4,19 +4,7 @@ import datetime
 import numpy as np
 import pandas as pd
 import math
-import time
+import json
 from binance.client import Client
 from binance.enums import *
 
-api_key = 'DYpf4c2nFdjIL8xIe2D4KckfRDEJJ9WD0N6bOJZBEvCD5IjhHGo6d1BCwXAR2bBV'
-api_secret = 'jUhYqtfcJABTj3mXX3VbhwCCpdgXcLbcGyoGrnOYSQZMMiYIJhSUudivydubucx1'
-client = Client(api_key, api_secret)
-
-order = client.create_test_order(
-    symbol='BNBBTC',
-    side=SIDE_BUY,
-    type=ORDER_TYPE_LIMIT,
-    timeInForce=TIME_IN_FORCE_GTC,
-    quantity=100,
-    price='0.001')
-print(order)
--- section1/task2/1.2.py
+++ section1/task2/1.2.py
@@ -3,10 +3,14 @@ import requests
 import datetime
 import numpy as np
 import pandas as pd
+import json
 from binance.enums import *
 
-api_key = 'DYpf4c2nFdjIL8xIe2D4KckfRDEJJ9WD0N6bOJZBEvCD5IjhHGo6d1BCwXAR2bBV'
-api_secret = 'jUhYqtfcJABTj3mXX3VbhwCCpdgXcLbcGyoGrnOYSQZMMiYIJhSUudivydubucx1'
+# load api
+with open('my_trading_api.json') as f:
+    api = json.load(f)
+    api_key = api[0]
+    api_secret = api[1]
 client = Client(api_key, api_secret)
 
 # get candle data
@@ -14,7 +18,8 @@ candles = client.get_klines(
     symbol='BNBBTC', interval=Client.KLINE_INTERVAL_30MINUTE)
 df_candles = pd.DataFrame(candles)
 df_candles[0] = pd.to_datetime(df_candles[0], unit='ms')
-df_candles.columns = ['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignored']
+df_candles.columns = ['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'Quote asset volume',
+                      'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignored']
 print('Candle data:')
 print(df_candles)
 df_candles.to_csv('section1/task2/candle.csv', index=False)
@@ -30,7 +35,8 @@ df_recent.to_csv('section1/task2/recent trades.csv', index=False)
 agg_trades = client.get_aggregate_trades(symbol='BNBBTC')
 df_agg = pd.DataFrame(agg_trades)
 # format
-df_agg.columns = ['Aggregated trade id', 'Price', 'Quantity', 'First breakdown trade id','Last breakdown trade id', 'Trade time', 'Maker', 'Ignored']
+df_agg.columns = ['Aggregated trade id', 'Price', 'Quantity', 'First breakdown trade id',
+                  'Last breakdown trade id', 'Trade time', 'Maker', 'Ignored']
 print('Aggregate trades')
 print(df_agg)
 df_agg.to_csv('section1/task2/aggregate trades.csv', index=False)
